17017b16a506e68fefa5ae74448cfa88505298a0
pchchv <ipchchv@gmail.com>
2022-10-07 11:43:44 +0300

    Implement a CheckGravatar method

--- gravatar.go
+++ gravatar.go
@@ -1,7 +1,56 @@
 package evt
 
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
 // Detail about the Gravatar
 type Gravatar struct {
 	HasGravatar bool   // Whether has gravatar
 	GravatarUrl string // Gravatar url
+}
+
+// Returns the Gravatar records for the given email
+func (v *Verifier) CheckGravatar(email string) (*Gravatar, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err, emailMd5 := getMD5Hash(strings.ToLower(strings.TrimSpace(email)))
+	if err != nil {
+		return nil, err
+	}
+	gravatarUrl := gravatarBaseUrl + emailMd5 + "?d=404"
+	req, err := http.NewRequest("GET", gravatarUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	// check body
+	err, md5Body := getMD5Hash(string(body))
+	if err != nil {
+		return nil, err
+	}
+	if md5Body == gravatarDefaultMd5 || resp.StatusCode != 200 {
+		return &Gravatar{
+			HasGravatar: false,
+			GravatarUrl: "",
+		}, nil
+	}
+	return &Gravatar{
+		HasGravatar: true,
+		GravatarUrl: gravatarUrl,
+	}, nil
 }
