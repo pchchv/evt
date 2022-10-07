7f111c10e982c70dd2051e9a629edb3e8f30b877
pchchv <ipchchv@gmail.com>
2022-10-08 01:39:16 +0300

    Implement a tests

---
+++ schedule_test.go
@@ -0,0 +1,52 @@
+package evt
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStartScheduleOK(t *testing.T) {
+	var ops uint32
+	f := func() {
+		atomic.AddUint32(&ops, 1)
+	}
+	s := newSchedule(time.Second, f)
+	s.start()
+	time.Sleep(time.Second * 3)
+	s.stop()
+	actual := atomic.LoadUint32(&ops)
+	assert.Equal(t, uint32(3), actual)
+	// assert.True(t, uint32(2) <= actual && actual <= uint32(3))
+}
+
+func TestNewScheduleOK(t *testing.T) {
+	f := func() {}
+	actual := newSchedule(time.Minute, f)
+	assert.NotNil(t, actual)
+}
+
+func TestNewScheduleOK_FuncWithParams(t *testing.T) {
+	f := func(a, b int) bool {
+		return a > b
+	}
+	actual := newSchedule(time.Minute, f, 3, 4)
+	assert.NotNil(t, actual)
+	assert.Equal(t, actual.funcParams, []interface{}{3, 4})
+}
+
+func TestNewScheduleWithWrongFunc(t *testing.T) {
+	f := "test"
+	actual := newSchedule(time.Minute, f)
+	assert.Nil(t, actual.funcParams)
+	assert.Equal(t, actual.handFunc, f)
+}
+
+func TestRunScheduleFailedWithWrongFunc(t *testing.T) {
+	f := "test"
+	actual := newSchedule(time.Minute, f)
+	assert.Equal(t, actual.handFunc, f)
+	assert.NotPanics(t, actual.start)
+}
