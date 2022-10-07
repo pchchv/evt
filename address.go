d4cf8723c13bf114aa044222ee1cf0da4ea5d003
pchchv <ipchchv@gmail.com>
2022-10-07 10:41:12 +0300

    Implement a ParseAddress method

--- address.go
+++ address.go
@@ -1,8 +1,36 @@
 package evt
 
+import (
+	"regexp"
+	"strings"
+)
+
+var emailRegex = regexp.MustCompile(emailRegexString)
+
 // Stores all information about an email Syntax
 type Syntax struct {
 	Username string `json:"username"`
 	Domain   string `json:"domain"`
 	Valid    bool   `json:"valid"`
+}
+
+// Attempts to parse an email address and return it in the form of a Syntax
+func (v *Verifier) ParseAddress(email string) Syntax {
+	isAddressValid := IsAddressValid(email)
+	if !isAddressValid {
+		return Syntax{Valid: false}
+	}
+	index := strings.LastIndex(email, "@")
+	username := email[:index]
+	domain := strings.ToLower(email[index+1:])
+	return Syntax{
+		Username: username,
+		Domain:   domain,
+		Valid:    isAddressValid,
+	}
+}
+
+// Checks if email address is formatted correctly by using regex
+func IsAddressValid(email string) bool {
+	return emailRegex.MatchString(email)
 }
--- evt.go
+++ evt.go
@@ -1,5 +1,11 @@
 package evt
 
+const (
+	emailRegexString = "^(?:(?:(?:(?:[a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(?:\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|(?:(?:\\x22)(?:(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(?:\\x20|\\x09)+)?(?:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(\\x20|\\x09)+)?(?:\\x22))))@(?:(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+	defaultFromEmail = "user@example.org"
+	defaultHelloName = "localhost"
+)
+
 // Email verifier
 // Create one by calling NewVerifier
 type Verifier struct {
@@ -25,11 +31,6 @@ type Result struct {
 	Free         bool      `json:"free"`           // Is domain a free email domain
 	HasMxRecords bool      `json:"has_mx_records"` // Whether MX-Records for the domain
 }
-
-const (
-	defaultFromEmail = "user@example.org"
-	defaultHelloName = "localhost"
-)
 
 // Creates a new email verifier
 func NewVerifier() *Verifier {
